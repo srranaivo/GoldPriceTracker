99a3a866cab23ebeada3905c7d6b76c1d48d8bcb
sophie ramahenina <ramahenina.sophie@gmail.com>
2024-06-02 18:27:31 +0200

    add index view

--- app/controllers/gold_prices_controller.rb
+++ app/controllers/gold_prices_controller.rb
@@ -1,4 +1,8 @@
 class GoldPricesController < ApplicationController
+  require 'httparty'
+
   def index
+    response = HTTParty.get("https://metals-api.com/api/latest?access_key=r5aez8535k2h9ch78as69a53boi5td4v4lj1vzglt26l99j04h3c56i9b1dk&base=USD&symbols=XAU")
+    @gold_price = response.parsed_response["rates"]["XAU"]
   end
 end
