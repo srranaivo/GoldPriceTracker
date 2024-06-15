d48290b68c1185c0180f56de2304686beeae3db5
sophie ramahenina <ramahenina.sophie@gmail.com>
2024-06-15 21:17:21 +0200

    using goldapi.io instead of openexchangerates.org

--- app/controllers/gold_prices_controller.rb
+++ app/controllers/gold_prices_controller.rb
@@ -2,17 +2,18 @@ class GoldPricesController < ApplicationController
   require 'httparty'
 
   def index
-    response = HTTParty.get("https://openexchangerates.org/api/latest.json?app_id=3f8b856ad62245ea8370fd5d9760c351")
+    response = HTTParty.get("https://www.goldapi.io/api/XAU/USD",
+                            headers: { "x-access-token" => "goldapi-32brhvcslxghksqm-io", "Content-Type" => "application/json" })
 
     # Afficher la réponse complète pour le débogage
     Rails.logger.info "API Response: #{response.parsed_response.inspect}"
 
     if response.success?
-      # L'API openexchangerates ne fournit pas directement le prix de l'or, mais on peut utiliser le taux de XAU par rapport à l'USD
-      if response.parsed_response["rates"] && response.parsed_response["rates"]["XAU"]
-        @gold_price = response.parsed_response["rates"]["XAU"]
+      @api_response = response.parsed_response
+      if @api_response["price"]
+        @gold_price = @api_response["price"]
       else
-        @error_message = "Rates not found in the API response: #{response.parsed_response}"
+        @error_message = "Price not found in the API response: #{@api_response.inspect}"
       end
     else
       @error_message = "Failed to fetch data from the API: #{response.code} - #{response.message}"
